6b40170f0128db628bf2f0e7f7992b38435455cc
rea-sna <reoshigeno0708@gmail.com>
2021-04-01 16:03:42 +0900

    Initial Commit

---
+++ AppStoreCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreCard
+//
+//  Created by 繁野怜央 on 2021/04/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
