a133aa02f5b23188aca657b7aacfc3eedc5da367
rea-sna <reoshigeno0708@gmail.com>
2021-04-01 16:19:02 +0900

    UP

--- AppStoreCard/ViewController.swift
+++ AppStoreCard/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import StoreKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func tappedButton(_ sender: UIButton) {
+        displayOverlay()
+    }
+    
+    func displayOverlay() {
+        guard let scene = view.window?.windowScene else { return }
 
-
+        let config = SKOverlay.AppConfiguration(appIdentifier: "375380948", position: .bottom)
+        let overlay = SKOverlay(configuration: config)
+        overlay.present(in: scene)
+    }
+   
+    
 }
 
